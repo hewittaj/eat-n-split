977ba0a73b1df1a805cb145f023731581e279188
Alex Hewitt <alexhewitt.j@gmail.com>
2024-02-02 08:39:18 -0600

    add controlled inputs

--- src/components/FormSplitBill.jsx
+++ src/components/FormSplitBill.jsx
@@ -1,26 +1,53 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react'
 import Button from './Button'
 
 function FormSplitBill(props) {
-    const { selectedFriend } = props
-    return (
-        <form className='form-split-bill'>
-            <h2>Split a bill with {selectedFriend.name}</h2>
-            <label>💵Bill value</label>
-            <input type='text' />
-            <label>🧔Your expense</label>
-            <input type='text' />
-            <label>🧛‍♂️{selectedFriend.name}'s expense</label>
-            <input type='text' disabled />
-            <label>🧙‍♂️Who is paying the bill?</label>
-            <select>
-                <option value='user'>You</option>
-                <option value='friend'>X</option>
-            </select>
+	const { selectedFriend } = props
+	const [bill, setBill] = useState('')
+	const [paidByUser, setPaidByUser] = useState('')
+	const paidByFriend = bill ? bill - paidByUser : ''
+	const [whoIsPaying, setWhoIsPaying] = useState('user')
 
-            <Button>Split bill</Button>
-        </form>
-    )
+	return (
+		<form className='form-split-bill'>
+			<h2>Split a bill with {selectedFriend.name}</h2>
+
+			<label>💵Bill value</label>
+			<input
+				type='text'
+				value={bill}
+				onChange={(e) => setBill(Number(e.target.value))}
+			/>
+
+			<label>🧔Your expense</label>
+			<input
+				type='text'
+				value={paidByUser}
+				onChange={(e) =>
+					setPaidByUser(
+						Number(e.target.value) > bill
+							? paidByUser
+							: Number(e.target.value)
+					)
+				}
+			/>
+
+			<label>🧛‍♂️{selectedFriend.name}'s expense</label>
+			<input type='text' disabled value={paidByFriend} />
+
+			<label>🧙‍♂️Who is paying the bill?</label>
+			<select
+				value={whoIsPaying}
+				onChange={(e) => setWhoIsPaying(e.target.value)}
+			>
+				<option value='user'>You</option>
+				<option value='friend'>{selectedFriend.name}</option>
+			</select>
+
+			<Button>Split bill</Button>
+		</form>
+	)
 }
 
 export default FormSplitBill
